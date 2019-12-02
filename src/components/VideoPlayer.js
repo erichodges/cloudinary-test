5b4ec08cac5fdc7b8b9aacbf643b1b67684a7f01
Eric Hodges <ehodges2004@gmail.com>
2019-12-02 14:15:10 -0800

    Layout: Fixed view width issue

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -6,7 +6,7 @@ const VideoPlayer = () => {
     <>
       {/* Cloudinary Video Player embed code */}
       <iframe
-        src="https://player.cloudinary.com/embed/?cloud_name=dn3baky3u&public_id=gatsby-cloudinary%2Fevija-dynamic-1-cut-1920&show_jump_controls=true&fluid=true&controls=true&muted=true&autoplay=true&autoplay_on_scroll=false&loop=true&source_types%5B0%5D=mp4"
+        src="https://player.cloudinary.com/embed/?cloud_name=dn3baky3u&public_id=gatsby-cloudinary%2Fevija-dynamic-1-cut-1920&fluid=true&controls=true&muted=true&autoplay=true&autoplay_on_scroll=false&loop=true&source_types%5B0%5D=mp4"
         allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
         allowfullscreen
         frameborder="0"
