05a758e7f9af0e345cfa46a6fce22c02a2739a72
Eric Hodges <ehodges2004@gmail.com>
2019-12-04 15:21:01 -0800

    Layout: Updated video params

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -6,8 +6,8 @@ const VideoPlayer = () => {
     <>
       {/* Cloudinary Video Player embed code */}
       <iframe
-        src="https://player.cloudinary.com/embed/?cloud_name=dn3baky3u&public_id=gatsby-cloudinary%2Fevija-dynamic-1-cut-1920&fluid=true&controls=true&muted=true&poster_options%5Btransformation%5D%5Bstart_offset%5D=0&autoplay=true&autoplay_on_scroll=false&loop=true&source_types%5B0%5D=mp4"
-        allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
+        src="https://player.cloudinary.com/embed/?cloud_name=dn3baky3u&public_id=gatsby-cloudinary%2Fevija-dynamic-1-cut-1920&fluid=true&controls=false&muted=true&poster_options%5Btransformation%5D%5Bstart_offset%5D=0&autoplay=true&autoplay_on_scroll=false&loop=true&source_types%5B0%5D=mp4"
+        allow="autoplay; fullscreen; encrypted-media; picture-in-picture; loop"
         allowfullscreen
         frameborder="0"
       ></iframe>
