4b06a40b8e6cbffe2a58599011d5dffd18c4df59
Eric Hodges <ehodges2004@gmail.com>
2019-12-06 14:16:54 -0800

    Layout: HTML video version

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,32 @@
-import { graphql, Link } from "gatsby"
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import VideoPlayer from "../components/VideoPlayer"
+import "../components/VideoPlayer.css"
 
-const styles = {
-  width: "400px",
-}
+// const styles = {
+//   width: "400px",
+// }
+
+// code to put in Layout for iframe videoPlayer
+// <VideoPlayer publicId="gatsby-cloudinary%2Fevija-dynamic-1-cut-1920" />
 const Index = ({ data }) => (
   <Layout>
-    <VideoPlayer publicId="gatsby-cloudinary%2Fevija-dynamic-1-cut-1920" />
+    <div className="carvideo">
+      <video controls autoPlay muted data-ar="1.778" playsInLine loop>
+        <source
+          src="https://77448790ecdce6d45774-3819184e75e4b8f6b5369812f0a4785e.ssl.cf3.rackcdn.com/evija/evija-dynamic-1-cut-1920.mp4"
+          type="video/mp4"
+          media="screen and (min-device-width:641px)"
+        />
+        <source
+          src="https://77448790ecdce6d45774-3819184e75e4b8f6b5369812f0a4785e.ssl.cf3.rackcdn.com/evija/evija-dynamic-1-cut-640.mp4"
+          type="video/mp4"
+          media="screen and (max-device-width:640px)"
+        />
+        If you can't see this video, you should update your browser. :-)
+      </video>
+    </div>
     <div>
       <SEO title="Page two" />
       <h1>Home page</h1>
