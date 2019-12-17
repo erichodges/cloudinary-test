01e1508112825e2c7c33f1bce0182353fd9f88ee
Eric Hodges <ehodges2004@gmail.com>
2019-12-17 14:14:22 -0800

    Chore: Importing car image from component

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,25 +1,8 @@
-import { graphql, Link, useStaticQuery } from "gatsby"
-import Image from "gatsby-image"
+import { Link } from "gatsby"
 import React from "react"
-// import CarImage from "../components/CarImage";
+import CarImage from "../components/CarImage"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-const CarImage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      image: file(name: { eq: "car" }) {
-        cloudinary: childCloudinaryAsset {
-          fluid(maxWidth: 1920) {
-            ...CloudinaryAssetFluid
-          }
-        }
-      }
-    }
-  `)
-
-  return <Image fluid={data.image.cloudinary.fluid} alt="car image" />
-}
 
 const PageTwo = () => (
   <Layout>
