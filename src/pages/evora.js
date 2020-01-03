f914dff085181bc3f19c1928bc2d71e006a743d8
Eric Hodges <ehodges2004@gmail.com>
2020-01-03 15:06:06 -0800

    Layout: Updated image import from cloudinary for evora gt page

--- src/pages/evora.js
+++ src/pages/evora.js
@@ -1,44 +1,82 @@
-import { graphql, Link, useStaticQuery } from "gatsby"
+import { graphql } from "gatsby"
 import Image from "gatsby-image"
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "../styles/evora.css"
 
-const CarImage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      image: file(name: { eq: "evora-gt-front" }) {
-        cloudinary: childCloudinaryAsset {
-          fluid(maxWidth: 1920) {
-            ...CloudinaryAssetFluid
-          }
-        }
-      }
-    }
-  `)
+// const CarImage = () => {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       image: file(name: { eq: "evora-gt-front" }) {
+//         cloudinary: childCloudinaryAsset {
+//           fluid(maxWidth: 1920) {
+//             ...CloudinaryAssetFluid
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  return <Image fluid={data.image.cloudinary.fluid} alt="car image" />
-}
+//   return <Image fluid={data.image.cloudinary.fluid} alt="car image" />
+// }
 
-const evora = () => (
+const evora = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <header>
-      <CarImage />
+      <Image fluid={data.evoraGT.cloudinary.fluid} />
     </header>
 
     <div className="main">
-      <h1 className="col-1">Evora GT</h1>
+      <h4 className="heading-A">Uniquely brilliant</h4>
 
-      <Link to="/" className="col-2">
-        Index page
-      </Link>
+      <p className="text-2">
+        <br />
+        <br />
+        The stunning new Evora GT takes the Evora’s unique attributes to
+        spectacular new heights. Sports car agility, precision and response
+        meets GT car refinement and comfort like never before.
+        <br />
+        <br />
+        First and foremost a pure driver’s car for the road, the Evora GT proves
+        that the value of motorsport engineering extends far beyond the race
+        track. Its functional aerodynamics, bonded aluminium chassis, Bilstein
+        dampers and spring set-up intelligently applied for road use. As a
+        result, focused driving is effortless with precise, intuitive steering
+        allied to huge reserves of mechanical grip, agility and poise. As the
+        twisting tarmac disappears beneath you, the Evora flows, settling into a
+        rhythm, smoothing out rough surfaces, breathing with the road for an
+        enthralling, immersive driving experience.
+      </p>
+      <Image fluid={data.evoraGTBack.cloudinary.fluid} />
+
       <br />
-      <h2 className="row-2">Some text placeholder</h2>
+      <h2 className="area-C">Some text placeholder</h2>
     </div>
     <div></div>
   </Layout>
 )
+
+export const fluidImage = graphql`
+  fragment fluidImage on File {
+    cloudinary: childCloudinaryAsset {
+      fluid(maxWidth: 1920) {
+        ...CloudinaryAssetFluid
+      }
+    }
+  }
+`
+
+export const query = graphql`
+  query {
+    evoraGT: file(name: { eq: "evora-gt-front" }) {
+      ...fluidImage
+    }
+    evoraGTBack: file(name: { eq: "evoraGT-back-square" }) {
+      ...fluidImage
+    }
+  }
+`
 
 export default evora
