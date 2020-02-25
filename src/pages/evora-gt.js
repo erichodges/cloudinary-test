f191c266759f83eb9061eeb96f7dac78813ddc19
Eric Hodges <ehodges2004@gmail.com>
2020-02-25 15:37:27 -0800

    Layout: Initial underline setup for heading on evora gt page

--- src/pages/evora-gt.js
+++ src/pages/evora-gt.js
@@ -12,9 +12,7 @@ const evora = ({ data }) => (
     </div>
     <div className="evoraGTSubmenu"></div>
     <div className="main">
-      <h4 className="heading area-A">Uniquely brilliant</h4>
-      <div className="underline"></div>
-
+      <h4 className="heading">Uniquely brilliant</h4>
       <p className="area-A">
         <br />
         <br />
