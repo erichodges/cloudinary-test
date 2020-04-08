307871ac0b010bd32749201162ad7a1034075d2c
Eric Hodges <ehodges2004@gmail.com>
2020-04-08 15:39:24 -0700

    Style: Updated Footer page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../styles/footer.css';
 const Footer = () => (
   <footer>
     <div className="footer-main">
-      <h3>Footer items here dude</h3>
+      <h3>Footer</h3>
     </div>
   </footer>
 )
