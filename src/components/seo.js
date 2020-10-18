37036ca03ee24256024b62e4eee9ddafdc1e6083
Eric Hodges <ehodges2004@gmail.com>
2020-10-18 15:35:03 -0700

    Chore: Fixed bug in seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -23,11 +23,7 @@ function SEO({ description, lang, meta, title }) {
         }
       }
     `
-  ).then((result) => {
-    if (result.errors) {
-      throw result.errors;
-    }
-  });
+  );
 
   const metaDescription = description || site.siteMetadata.description;
 
